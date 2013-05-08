94d8751a33c658ab39ae1dfb9369a9b3b82c0474
Mark Benny <1markbenny@gmail.com>
2013-05-08 23:55:53 +1000

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                for (int m = 0; m <= 100; m++)
+                {
+                    if (m % 3 == 0 && m % 5 == 0)
+                        Console.WriteLine("FizzBuzz");
+                    else if (m % 3 == 0)
+                        Console.WriteLine("Fizz");
+                    else if (m % 3 == 0)
+                        Console.WriteLine("Buzz");
+                    else
+                        Console.WriteLine(m);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
